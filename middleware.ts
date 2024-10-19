2bd1f2de53c07f9e1cff7a753199d0361378c49f
Moumouuu <pluviauxweb@gmail.com>
2024-10-19 16:24:34 +0200

    feat(auth): add middleware

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { authMiddleware } from "better-auth/next-js";
+
+export default authMiddleware({
+  redirectTo: "/login", // redirect to this path if the user is not authenticated
+});
+
+export const config = {
+  matcher: ["/((?!login|register).)*"],
+};
